6242fea6fc0f1398abe980b6e17c55f81a0cdfbd
Yang Liu <foamliu@yeah.net>
2018-10-09 17:10:29 +0800

    update

--- config.py
+++ config.py
@@ -1,3 +1,20 @@
 import torch
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Configure training/optimization
+learning_rate = 0.0001
+hidden_size = 500
+encoder_n_layers = 2
+min_word_freq = 3
+print_every = 100
+dropout = 0.05
+batch_size = 200
+
+train_folder = 'data/ai_challenger_sentiment_analysis_trainingset_20180816'
+valid_folder = 'data/ai_challenger_sentiment_analysis_validationset_20180816'
+test_a_folder = 'data/ai_challenger_sentiment_analysis_testa_20180816'
+
+train_filename = 'sentiment_analysis_trainingset.csv'
+valid_filename = 'sentiment_analysis_validationset.csv'
+test_a_filename = 'sentiment_analysis_testa.csv'
--- pre_process.py
+++ pre_process.py
@@ -1,2 +1,37 @@
+import json
+import os
+from collections import Counter
+
+from tqdm import tqdm
+
+from config import *
+
+
+def build_wordmap():
+    filename = os.path.join(train_folder, train_filename)
+
+    with open(filename, 'r') as f:
+        sentences = f.readlines()
+
+    word_freq = Counter()
+
+    for sentence in tqdm(sentences):
+        # Update word frequency
+        word_freq.update(list(sentence.strip()))
+
+    # Create word map
+    words = [w for w in word_freq.keys() if word_freq[w] > min_word_freq]
+    word_map = {k: v + 4 for v, k in enumerate(words)}
+    word_map['<pad>'] = 0
+    word_map['<start>'] = 1
+    word_map['<end>'] = 2
+    word_map['<unk>'] = 3
+    print('len(word_map): ' + str(len(word_map)))
+    print(words[:10])
+
+    with open('data/WORDMAP.json', 'w') as file:
+        json.dump(word_map, file, indent=4)
+
+
 if __name__ == '__main__':
-    None
+    build_wordmap()
