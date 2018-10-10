687ee1101f733988cb86b50756298f6c318ad560
Yang Liu <foamliu@yeah.net>
2018-10-10 19:52:55 +0800

    update

--- utils.py
+++ utils.py
@@ -81,5 +81,18 @@ def encode_text(word_map, c):
     return [word_map.get(word, word_map['<unk>']) for word in c] + [word_map['<end>']]
 
 
-def accuracy(y_pred, y_true):
-    return 0
+def accuracy(scores, targets):
+    """
+    Computes top-k accuracy, from predicted and true labels.
+    :param scores: scores from the model
+    :param targets: true labels
+    :param k: k in top-k accuracy
+    :return: top-k accuracy
+    """
+
+    batch_size = targets.size(0)
+    _, ind = scores.topk(k, 1, True, True)
+    correct = ind.eq(targets.view(-1, 1).expand_as(ind))
+    correct_total = correct.view(-1).float().sum()  # 0D tensor
+    return correct_total.item() * (100.0 / batch_size)
+
