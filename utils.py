d8f19ba6d493bce99f14c77408f1f970bc26c35f
Yang Liu <foamliu@yeah.net>
2018-10-11 16:32:22 +0800

    update

--- utils.py
+++ utils.py
@@ -20,6 +20,27 @@ class Lang:
         self.n_words = len(word_map)
 
 
+class AverageMeter(object):
+    """
+    Keeps track of most recent, average, sum, and count of a metric.
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
 # Exponentially weighted averages
 class ExpoAverageMeter(object):
     # Exponential Weighted Average Meter
